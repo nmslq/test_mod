7ed47fc6f87a9e0824a5ef28f2b1cb622af3b9aa
nmslq <96362104+nmslq@users.noreply.github.com>
2023-06-22 18:28:53 +0800

    Update create.lua

--- scripts/create.lua
+++ scripts/create.lua
@@ -5,3 +5,9 @@ function onCreate()
     setProperty('healthBar.alpha', 0);
     debugPrint(abc[2]);
 end
+
+function onStepHit()
+    if curStep == 5 then
+        doTweenAlpha('tw', 'note', 0, 1, 'linear');
+    end
+end
