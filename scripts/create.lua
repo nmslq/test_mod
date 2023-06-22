c168aeb4a4d3426c68f3219e44bc4c3cf144bdfa
nmslq <96362104+nmslq@users.noreply.github.com>
2023-06-22 19:14:54 +0800

    Update create.lua

--- scripts/create.lua
+++ scripts/create.lua
@@ -4,10 +4,19 @@ function onCreate()
     for i = 1, 3 do
         setProperty(item[i] .. '.alpha', 0);
     end
+
+    runHaxeCode([[
+        var text = new Alphabet(0, game.healthBar.y + 50, 'url lua version', true);
+        text.cameras = [game.camHUD];
+        text.alpha = 0.75;
+        game.add(text);
+        setVar('alp', text);
+    ]]);
 end
 
 function onStepHit()
     if curStep == 5 then
         doTweenAlpha('tw', 'note', 0, 1, 'linear');
+        debugPrint('URL change!!!');
     end
 end
